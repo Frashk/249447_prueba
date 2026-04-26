992c630da60c3f2278a37de69a5e11dd4a765ba1
Frashk <julval_09@hotmail.com>
2026-04-25 21:37:51 -0500

    subir clase 3

---
+++ contracts/4_Intro.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts@4.9.3/utils/Counters.sol";
+
+contract intro{
+    uint256 private cantidad;
+    string public nombre ="Upao";
+
+    address public cuentaInicial;
+    address public cuentaUpdate;
+
+    constructor (uint256 _valorCantidad) {
+        cantidad = _valorCantidad;
+        cuentaInicial = msg.sender;
+    }
+
+    function cambiarCantidad(uint256 _cantidad) public {
+        cantidad = _cantidad;
+        cuentaUpdate = msg.sender;
+    }
+
+    function obtenerCantidad() public view returns (uint256){
+        return cantidad;
+
+    }
+}
